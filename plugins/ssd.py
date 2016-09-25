b4d2b54fe7c21bc285357d94ed236385981e436a
wxmann <wxfreakk91@gmail.com>
2016-09-24 23:18:42 -0700

    SSD plugin: start using config file to drive save settings

--- plugins/ssd.py
+++ plugins/ssd.py
@@ -1,18 +1,34 @@
+import configparser
+from datetime import timedelta
 from core import source, target, saver
 
 __author__ = 'tangz'
 
-_DATEFORMAT = '%Y%m%d_%H%M%S'
+_OUTPUT_DATEFORMAT = '%Y%m%d_%H%M%S'
 
 
-def save_ssd_animated(storm_id, sattypes, saveloc, saveperiod, begin=None, end=None):
+def load_config(file):
+    config = configparser.ConfigParser()
+    config.read(file)
+    props = config['SSD']
+
+    stormid = props['StormId']
+    types = props['Types'].split(',')
+    hrs = props.getint('Hours')
+    outputdir = props['Dir']
+
+    saveperiod = timedelta(hours=hrs)
+    return _ssd_context_animated(stormid, types, outputdir, saveperiod)
+
+
+def _ssd_context_animated(storm_id, sattypes, saveloc, saveperiod):
     mutator = lambda file, timestamp: "-".join(
-        [storm_id, file.replace("-animated", ""), timestamp.strftime(_DATEFORMAT)])
-    context = saver.Session().create_context(storm_id, source.singular(), target.copyfilename(mutator),
-                                             saveperiod=saveperiod)
+        [storm_id, file.replace("-animated", ""), timestamp.strftime(_OUTPUT_DATEFORMAT)])
+    srcsetting = source.singular()
+    targsetting = target.copyfilename(mutator)
+    context = saver.Context(srcsetting, targsetting, saveperiod=saveperiod)
     for sattype in sattypes:
         url = "http://www.ssd.noaa.gov/PS/TROP/floaters/{0}/imagery/{1}-animated.gif".format(storm_id, sattype.lower())
         jobid = '-'.join([storm_id, sattype])
         context.submit(jobid, url, saveloc)
-    context.runall(begin, end)
     return context
